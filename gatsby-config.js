f6db90a777eafef01e8da329f2b40cece379ed67
catd825 <60204063+catd825@users.noreply.github.com>
2023-02-26 15:42:20 -0500

    prettier in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,19 +11,14 @@ module.exports = {
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
           themeColor: "#000000", // Used in manifest.json
-          display: "minimal-ui", // Used in manifest.json
+          display: "minimal-ui" // Used in manifest.json
         },
         contentDirectory: "./content",
         blogSettings: {
           path: "/blog", // Defines the slug for the blog listing page
-          usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
-        },
-        // googleAnalytics: {
-        //     trackingId: "UA-XXXXXX-X",
-        //     anonymize: true, // Default true
-        //     environments: ["production", "development"] // Default ["production"]
-        // }
-      },
-    },
-  ],
+          usePathPrefixForArticles: false // Default true (i.e. path will be /blog/first-article)
+        }
+      }
+    }
+  ]
 };
